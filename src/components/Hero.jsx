1b01ac635b4d88c7f4ffcef1e98eb755c8968a24
Arkaive19 <arkaive19@gmail.com>
2025-11-06 13:23:52 +0545

    Completed Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import verticalLogo from "../assets/verticalLogo.png";
 const Hero = () => {
-  return <div className="hero-section">!</div>;
+  return (
+    <div className="hero-section centered-flex">
+      <div className="card centered-flex">
+        <h1>Mangsir 21-22</h1>
+        <img src={verticalLogo} className="hero-img" alt="" />
+        <button className="btn-alt">Apply Now!</button>
+      </div>
+    </div>
+  );
 };
 export default Hero;
